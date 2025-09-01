d6684fc60bbab19c5eb7c7badbef2775f8434764
T1desLuck <tidesluck@icloud.com>
2025-09-01 17:39:26 +0500

    Delete src/datasets directory

--- src/datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .simple_dataset import SimpleColorizationDataset
-from .advanced_dataset import AdvancedColorizationDataset
-
-__all__ = ["SimpleColorizationDataset", "AdvancedColorizationDataset"]
